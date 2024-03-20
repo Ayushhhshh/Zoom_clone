a94c64d7ea00c91a20979c2d0ae2e5e02460d3af
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-21 03:33:58 +0530

    First Commit

--- lib/login_ui.dart
+++ lib/login_ui.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
-class LoginUI extends StatelessWidget {
+class LoginUI extends StatefulWidget {
   const LoginUI({super.key});
 
   @override
+  State<LoginUI> createState() => _LoginUIState();
+}
+
+class _LoginUIState extends State<LoginUI> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black,),
+      appBar: AppBar(backgroundColor: Colors.black,title: const Text("Login Page", style: TextStyle(color: Colors.white),),),
+      body: const Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: TextField(cursorRadius: Radius.circular(10),
+            style: TextStyle(color: Colors.white),
+            ),
+          ),SizedBox(
+            height:1,
+          ),
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: TextField(cursorRadius: Radius.circular(10),),
+          )
+        ],
+      ),
+      
     );
   }
 }

36ead5ca0be3e114dfa27d31636824240899c503
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-22 02:24:34 +0530

    Firebase added

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:zoom_clone/utils/utils.dart';
 
 class AuthMethods{
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  signInWithGoogle() async{
+  Future <bool> signInWithGoogle(BuildContext context) async{
+     bool res = false;
     try{
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
@@ -23,15 +26,19 @@ class AuthMethods{
 
     if(user!=null){
       if(userCredential.additionalUserInfo!.isNewUser) {
-         _firestore.collection('users').doc(user.uid).set({
+         await _firestore.collection('users').doc(user.uid).set({
           'username':user.displayName,
           'uid':user.uid,
           'profilePhoto':user.photoURL
          });
       }
+      res = true;
     }
 
-    } catch(e){ 
+    } on FirebaseAuthException catch(e){ 
+      showSnackBar(context, e.message!);
+      res = false; 
     }
+    return res;
   }
 }
